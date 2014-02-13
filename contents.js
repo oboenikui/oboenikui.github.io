b7d7ded2e2252d6093077b9df59f657b2b051046
oboenikui <oboenikuitwitter@gmail.com>
2014-02-14 03:18:59 +0900

    change icons

---
+++ contents.js
@@ -0,0 +1,29 @@
+IMG_PATH = "images/";
+COLOR_FOOTER = "_color.png"
+GRAY_FOOTER = "_gray.png";
+function mouseOverAction(c){
+  var parent = c.parentElement;
+  var newImageName = parent.id+COLOR_FOOTER;
+  c.src = IMG_PATH+newImageName;
+}
+
+function mouseOutAction(c){
+  var parent = c.parentElement;
+  var newImageName = parent.id+GRAY_FOOTER;
+  c.src = IMG_PATH+newImageName;
+}
+
+function onWindowLoad() {
+  var links = document.getElementById("side_bar").querySelectorAll("a");
+  for(var i=0;i<links.length;i++){
+    var link = links[i];
+    var imageName = link.id+GRAY_FOOTER;
+    var img = document.createElement("img");
+    img.setAttribute("src", IMG_PATH+imageName);
+    img.setAttribute("alt", link.textContent);
+    img.setAttribute("onmouseover", "mouseOverAction(this)");
+    img.setAttribute("onmouseout", "mouseOutAction(this)");
+    link.textContent="";
+    link.appendChild(img);
+  }
+}
